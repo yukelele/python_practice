f369ad0549ff3c9869591a48cd9aa378630d1595
Yuki Mano <ysm@umail.ucsb.edu>
2018-08-01 15:40:16 -0700

    array is a subset of another subset

---
+++ array_is_a_subset_of_another_subset.py
@@ -0,0 +1,17 @@
+def isSubset(arr1, arr2):
+    #m = len(arr1)
+    #n = len(arr2)
+    hash = {}
+    for i in arr1:
+        hash[i] = i
+
+    for j in arr2:
+        if not (j in hash):
+            return False
+    
+    return True
+
+arr1 = [11,1,13,21,3,7]
+arr2 = [11,3,7,1]
+
+print(isSubset(arr1,arr2))  # TRUE
